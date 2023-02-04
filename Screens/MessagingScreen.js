04c700761a1a056a1cbce7f32ddfaa23cdffaa30
danicaj3w <104111962+danicaj3w@users.noreply.github.com>
2023-02-04 14:08:01 -0800

    Messaging Screen created

--- Screens/MessagingScreen.js
+++ Screens/MessagingScreen.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>According to all known laws of aviation, there is no way a bee should be able to fly.</Text>
-      <Text>It's tiny little wings should not be able to lift its fat body into the air.</Text>
+      <Text>This is your app.</Text>
       <StatusBar style="auto" />
     </View>
   );
